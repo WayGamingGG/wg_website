a9b195760715f61df915c5baa28b36f2fa7912a3
jeffersonbergman <jefferson.bergman@yahoo.com>
2026-02-05 18:59:56 +0000

    fix2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Splash from "./pages/Splash";
 import Index from "./pages/Index";
@@ -31,7 +31,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Splash />} />
             <Route path="/wg" element={<Index />} />
@@ -51,7 +51,7 @@ const App = () => (
             <Route path="/wg-colosseum/regras" element={<WGRules />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </HelmetProvider>
