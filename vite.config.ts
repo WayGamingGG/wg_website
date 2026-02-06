cbded265c4edceb08b413f4ddcf650f2f00bd71a
jeffersonbergman <jefferson.bergman@yahoo.com>
2026-02-06 13:25:07 +0000

    new

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/wg_website/",
+  base: "/",
 
   server: {
     host: "::",
